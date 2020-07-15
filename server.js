0caa2c33a9a514399d6d26f9eac8151c675dcfa5
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-15 13:53:34 -0400

    Cmt. 1 - Renders seats based on flight#

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { flights } = require('./test-data/flightSeating');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { flights } = require("./test-data/flightSeating");
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
   // endpoints
+  .get("/flights/:flightId", (req, res) => {
+    const flightNumber = req.params.flightId;
+    const currentFlight = flights[flightNumber];
+    res.status(200).send(currentFlight);
+  })
 
-  .use((req, res) => res.send('Not Found'))
+  .use((req, res) => res.send("Not Found"))
   .listen(8000, () => console.log(`Listening on port 8000`));
